a6e71da6433ebbc30e88d5834ac00ca30cd53e88
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2024-09-21 14:10:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function mincost(arr)
 { 
 //write your code here
+	let cost = 0;
+	arr.sort((a,b) => a-b);
+	while(arr.length >1 ){
+		let first = arr.shift();
+		let second = arr.shift();
+		cost += first+second;
+
+		arr.sort((a,b) => a-b);
+	}
 // return the min cost
-  
+  return cost;
 }
 
 module.exports=mincost;
